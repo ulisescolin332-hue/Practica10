30b684468548ef49c8643476d8debcca705374b2
Uli__ <ulisescolin332@gmail.com>
2025-11-07 20:03:23 -0600

    Create Programa2c.c

---
+++ Programa2c.c
@@ -0,0 +1,24 @@
+/* Francisco Colín Ulises 
+Práctica 10, Programa 2c*/
+
+#include<stdio.h>
+int main()
+{
+     int i,j,a[5][5];
+     i=0;
+     do //Representa al renglón del arreglo
+     {
+         j=0;
+         do //Representa a la columna del arreglo
+         {
+             a[i][j]=i+j;
+             printf("\t%d, ",a[i][j]);
+             j++;
+         }
+         while (j<5);
+         printf("\n");
+         i++;
+     }
+     while (i<5);
+     return 0;
+}
