2fee2a6f217c8525638f8679f02170fd6d001336
Uli__ <ulisescolin332@gmail.com>
2025-11-07 19:20:48 -0600

    Create Programa1a.c

---
+++ Programa1a.c
@@ -0,0 +1,19 @@
+/* Francisco Colín Ulises 
+Práctica 10, Programa 1a*/
+
+#include<stdio.h>
+int main()
+{
+    int matriz[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int i, j;
+    printf("Imprimir Matriz\n");
+    for (i=0 ; i<3 ; i++) //Representa al renglón del arreglo
+    {
+        for (j=0 ; j<3 ; j++)//Representa a la columna del arreglo
+        {
+            printf("%d, ",matriz[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
